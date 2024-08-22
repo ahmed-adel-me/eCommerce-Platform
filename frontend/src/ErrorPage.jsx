6238d112142867a87f0223a72d856e7f9a86de1b
Ahmed Adel <ahmed.adel.dev@hotmail.com>
2024-08-22 18:01:42 +0300

    Fixed ErrorPage

--- frontend/src/ErrorPage.jsx
+++ frontend/src/ErrorPage.jsx
@@ -1,18 +1,13 @@
-import { useRouteError } from "react-router-dom";
 import Navbar from "./routes/root/Navbar";
 
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
   return (
     <>
       <Navbar />
       <div>
         <h1>Oops!</h1>
         <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{error.statusText || error.message}</i>
-        </p>
+        <p></p>
       </div>
     </>
   );

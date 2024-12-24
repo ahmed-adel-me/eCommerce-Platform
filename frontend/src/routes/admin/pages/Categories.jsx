1c88c08de410787941e15d15ec2eaa5201ec5f64
Ahmed Adel <ahmed.adel.dev@hotmail.com>
2024-12-24 03:22:11 +0200

    Updated category edit

--- frontend/src/routes/admin/pages/Categories.jsx
+++ frontend/src/routes/admin/pages/Categories.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import CategoriesTable from "../features/categories/CategoriesTable";
 import CreateCategory from "../features/categories/CreateCategory";
+import EditCategory from "../features/categories/EditCategory";
 
 function Categories() {
-  return (
-    <div>
-      <h2 className="text-2xl font-semibold mb-5">Categories</h2>
-      <CreateCategory />
-      <CategoriesTable />
-    </div>
-  );
+  const [editedCategory, setEditedCategory] = useState("");
+  if (editedCategory)
+    return (
+      <EditCategory
+        categoryId={editedCategory}
+        setEditedCategory={setEditedCategory}
+      />
+    );
+  else
+    return (
+      <div>
+        <h2 className="text-2xl font-semibold mb-5">Categories</h2>
+        <CreateCategory />
+        <CategoriesTable setEditedCategory={setEditedCategory} />
+      </div>
+    );
 }
 
 export default Categories;
